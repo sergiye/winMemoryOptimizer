410c524b3a64b45600b80ff7b8afedf51b8a12f1
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2025-08-29 09:33:57 +0300

    Crasher fix (autostart in RDP session issue)

--- TrayRAMBooster/Program.cs
+++ TrayRAMBooster/Program.cs
@@ -10,39 +10,33 @@ namespace TrayRAMBooster {
       
       Crasher.Listen();
 
-      try {
-
-        if (!OperatingSystemHelper.IsCompatible(true, out var errorMessage, out var fixAction)) {
-          if (fixAction != null) {
-            if (MessageBox.Show(errorMessage, Updater.ApplicationName, MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes) {
-              fixAction.Invoke();
-            }
+      if (!OperatingSystemHelper.IsCompatible(true, out var errorMessage, out var fixAction)) {
+        if (fixAction != null) {
+          if (MessageBox.Show(errorMessage, Updater.ApplicationName, MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes) {
+            fixAction.Invoke();
           }
-          else {
-            MessageBox.Show(errorMessage, Updater.ApplicationName, MessageBoxButtons.OK, MessageBoxIcon.Warning);
-          }
-          Environment.Exit(0);
         }
-
-        if (WinApiHelper.CheckRunningInstances(true, false)) {
-          MessageBox.Show($"{Updater.ApplicationName} is already running.", Updater.ApplicationName,
-            MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-          return;
+        else {
+          MessageBox.Show(errorMessage, Updater.ApplicationName, MessageBoxButtons.OK, MessageBoxIcon.Warning);
         }
-
-        // if (Environment.Is64BitOperatingSystem != Environment.Is64BitProcess) {
-        //   if (MessageBox.Show("You are running an application build made for a different OS architecture.\nIt is not compatible!\nWould you like to download correct version?", Updater.ApplicationName, MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes) {
-        //     Updater.VisitAppSite("releases");
-        //   }
-        //   Environment.Exit(0);
-        // }
-
-        var applicationContext = new TrayApplicationContext();
-        Application.Run(applicationContext);
+        Environment.Exit(0);
       }
-      catch (Exception) {
-        //Logger.Instance().Err(ex.Message);
+
+      if (WinApiHelper.CheckRunningInstances(true, false)) {
+        MessageBox.Show($"{Updater.ApplicationName} is already running.", Updater.ApplicationName,
+          MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        return;
       }
+
+      // if (Environment.Is64BitOperatingSystem != Environment.Is64BitProcess) {
+      //   if (MessageBox.Show("You are running an application build made for a different OS architecture.\nIt is not compatible!\nWould you like to download correct version?", Updater.ApplicationName, MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes) {
+      //     Updater.VisitAppSite("releases");
+      //   }
+      //   Environment.Exit(0);
+      // }
+
+      var applicationContext = new TrayApplicationContext();
+      Application.Run(applicationContext);
     }
   }
 }
